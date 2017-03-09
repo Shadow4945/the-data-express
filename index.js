adc46e9336bc9b77641d5a7877872e1557e5f4af
Daniel Carapia <toonlink4943@gmail.com>
2017-03-09 03:39:51 -0700

    Added all routes and pug files

--- index.js
+++ index.js
@@ -6,3 +6,22 @@ var express = require('express'),
     bcrypt = require('bcrpyt-nodejs'),
     cookieParser = require('cookie-parser')
     expressSessions = require('express-sessions');
+
+var app = express();
+
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+
+app.use(express.static(path.join(__dirname + '/public')));
+
+var urlencodedParser = bodyParser.urlencoded({extended: true})
+
+    app.get('/', route.index);
+    app.get('/admin/:id', route.admin);
+    app.get('/createAccount', route.create);
+    app.post('/createAccount', urlencodedParser, route.createAccount);
+    app.get('edit/:id', route.edit);
+    app.post('/edit/:id', urlencodedParser, route.editAccount);
+    app.get('/delete/:id', route.delete);
+
+app.listen(3000);
--- routes/routes.js
+++ routes/routes.js
@@ -7,3 +7,34 @@ mdb.on('error', console.error.bind(console, 'connection error:'));
 mdb.once('open', function (callback) {
     
 });
+
+exports.index = function(req, res){
+    res.render('index');
+}
+exports.create = function(req, res){
+    res.render('createAcc');
+}
+
+exports.admin = function(req,res){
+    res.render('admin');
+}
+
+exports.createAccount = function(req, res){
+    //CRUD here
+    res.redirect('/');
+}
+
+exports.edit = function(req, res){
+    //CRUD
+    res.render('edit');
+}
+
+exports.editAccount = function(req,res){
+    //CRUD
+    res.redirect('/');
+}
+
+exports.delete = function(req, res){
+    //CRUD
+    res.redirect('/');
+}
