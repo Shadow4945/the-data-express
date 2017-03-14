fa57f31bababf2ea282f0a7e3a2744b62464888d
Daniel Carapia <toonlink4943@gmail.com>
2017-03-14 04:19:38 -0600

    All I need is charts

--- index.js
+++ index.js
@@ -5,25 +5,28 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     bcrypt = require('bcrypt-nodejs'),
     cookieParser = require('cookie-parser')
-    expressSessions = require('express-sessions');
+    expressSessions = require('express-session');
 
 var app = express();
 
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname + '/public')));
+app.use(expressSessions({secret:'ihavenoideawhatimdoing'}));
 
 var urlencodedParser = bodyParser.urlencoded({ extended: true})
 
     app.get('/', route.index);
     app.get('/login', route.login);
     app.post('/login',urlencodedParser, route.loggedIn);
-    app.get('/admin/:id', route.admin);
+    app.get('/admin', route.admin);
     app.get('/createAccount', route.create);
     app.post('/createAccount', urlencodedParser, route.createAccount);
     app.get('/edit/:id', route.edit);
     app.post('/edit/:id', urlencodedParser, route.editAccount);
     app.get('/delete/:id', route.delete);
+    app.get('/logout', route.logout);
 
 app.listen(3000);
--- routes/routes.js
+++ routes/routes.js
@@ -9,6 +9,10 @@ mdb.once('open', function (callback) {
 });
 var bcrypt = require('bcrypt-nodejs');
 var hash;
+// var ctx1 = document.getElementById('question1').getContext("2d");
+// var ctx2 = document.getElementById('question2').getContext("2d");
+// var ctx3 = document.getElementById('question3').getContext("2d");
+
 
 var userSchema = mongoose.Schema({
     username: String,
@@ -25,13 +29,74 @@ var User = mongoose.model('Data_Express_Collection', userSchema);
 
 
 exports.index = function (req, res) {
-    User.find(function (err, _user) {
+    var currentUser;
+    if(req.session.username){
+        if(req.session.username == "admin"){
+        User.find(function (err, _user) {
+        if (err) return console.error(err);
+        for(var i = 0; i < _user.length; i++){
+            if(_user[i].username == req.session.username){
+                currentUser = _user[i]._id;
+            }   
+        }
+        //console.log(currentUser);
+        res.render('index', {
+            navbar1: "div class='navbar'",
+            navbar2: "ul",
+            navbar3: "li",
+            navbar4: "a href='/'",
+            navbar5: "Home",
+            navbar6: "a href='/admin'",
+            navbar7: "Admin Page",
+            navbar8: "a href='/edit/" + currentUser + "'",
+            navbar9: "Edit",
+            navbar10: "a href='/logout'",
+            navbar11: "Logout",
+            users: _user,
+            cookie: "Last Visited: " + req.session.lastVisited
+        });
+    });
+    }else{
+        User.find(function (err, _user) {
+        if (err) return console.error(err);
+        for(var i = 0; i < _user.length; i++){
+            if(_user[i].username == req.session.username){
+                currentUser = _user[i]._id;
+            }   
+        }
+        res.render('index', {
+            navbar1: "div class='navbar'",
+            navbar2: "ul",
+            navbar3: "li",
+            navbar4: "a href='/'",
+            navbar5: "Home",
+            navbar6: "a href='/edit/" + currentUser + "'",
+            navbar7: "Edit",
+            navbar8: "a href='/logout'",
+            navbar9: "Logout",
+            users: _user,
+            cookie: "Last Visited: " + req.session.lastVisited
+        });
+    });
+    }
+    }else{
+        User.find(function (err, _user) {
         if (err) return console.error(err);
         res.render('index', {
-            title: 'Users',
+            navbar1: "div class='navbar'",
+            navbar2: "ul",
+            navbar3: "li",
+            navbar4: "a href='/'",
+            navbar5: "Home",
+            navbar6: "a href='/login'",
+            navbar7: "Login",
+            navbar8: "a href='/createAccount'",
+            navbar9: "Create Account",
             users: _user
         });
     });
+    }
+    
 }
 exports.create = function (req, res) {
     res.render('createAcc');
@@ -59,9 +124,9 @@ exports.createAccount = function (req, res) {
         access_level: req.body.access_level,
         email: req.body.email,
         age: req.body.age,
-        answer1: req.body.answer1,
-        answer2: req.body.answer2,
-        answer3: req.body.answer3
+        answer1: req.body.colorQ,
+        answer2: req.body.degreeQ,
+        answer3: req.body.petQ
     });
 
      _user.save(function (err, _user) {
@@ -89,10 +154,9 @@ exports.edit = function (req, res) {
     User.findById(req.params.id, function (err, _user) {
         if (err) return console.error(err);
         res.render('edit', {
-            _user: _user
+            user: _user
         });
     });
-    res.render('edit');
 }
 
 exports.editAccount = function (req, res) {
@@ -131,25 +195,41 @@ exports.delete = function (req, res) {
 }
 
 exports.login = function (req, res) {
+    if(req.session.username){
+        console.log("WAT IS GOING ON");
+    }
     res.render('login');
 }
 
 exports.loggedIn = function (req, res) {
+    var j;
+    var rn = new Date();
+    rn = rn.getFullYear()+'-'+(rn.getMonth()+1)+'-'+rn.getDate()+ ' ' + rn.getHours() + ":"+rn.getMinutes()+":"+rn.getSeconds();
+    req.session.username = req.body.username;
+    req.session.lastVisited = req.session.visited || rn;
+    req.visited = rn;
     User.find(function (err, _users) {
         if (err) return console.error(err);
         for (var i = 0; i < _users.length; i++) {
             if (_users[i].username == req.body.username) {
-                console.log(_users[i].password);
+                //console.log(_users[i].password);
+                j = i;
                 bcrypt.compare(req.body.password, _users[i].password, function (err, res) {
                     if (res) {
-                        //console.log(_users[i].password);
-                    } else {
-
+                        console.log(_users[j].access_level);
+                        req.session.username = _users[j].access_level;
                     }
                 });
-
+                
             }
         }
+    });
+    res.redirect('/');
+}
+
+exports.logout = function(req,res){
+    req.session.destroy(function(err){
+
     });
     res.redirect('/');
 }
